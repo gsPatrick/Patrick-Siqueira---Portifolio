e13a02e1457bc324dd93e99587a6f694e737c2e6
unknown <patrickgsiqueira@hotmail.com>
2024-12-14 20:52:30 -0300

    Mudança no codigo

--- src/components/Preference.jsx
+++ src/components/Preference.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import '../styles/Preference.css';
-import "../styles/responsive.css";
+import '../styles/Preference.css'; // Aqui já estamos importando o CSS
 
 const Preference = () => {
-    return (
-      <div className="preference-container">
-        <h1 className="preference-title" style={{ color: '#C69AFF' }}>Javascript</h1>
-        <h1 className="preference-title" style={{ color: '#9F53FF' }}>React</h1>
-        <h1 className="preference-title" style={{ color: '#6104D6' }}>Coffee</h1>
-      </div>
-    );
-  };
-  
-  export default Preference;
+  return (
+    <div className="preference-container">
+      <h1 className="preference-title js">Javascript</h1>
+      <h1 className="preference-title react">React</h1>
+      <h1 className="preference-title coffee">Coffee</h1>
+    </div>
+  );
+};
+
+export default Preference;
