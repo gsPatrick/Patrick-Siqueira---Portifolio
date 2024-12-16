aac9e3d84b4004ce354aae5e57e708ac38a56690
unknown <patrickgsiqueira@hotmail.com>
2024-12-16 14:13:45 -0300

    Atualizações na página

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/AboutMe.css';
 import Nobres from '../assets/Empresas/Nobres.png'; // Caminho da logo
+import PatrickDeveloper from '../assets/Empresas/placeholder.png'
 
 // Definindo os dados dos cards como constantes
 const aboutMeData = [
@@ -17,6 +18,17 @@ const aboutMeData = [
     
     Além disso, desenvolvo sistemas, páginas interativas e quizzes personalizados, focando na experiência do usuário e no impacto visual. 
     Atualmente, também estou me especializando em tecnologias emergentes como Blockchain, Web 3.0 e criptoativos, ampliando meu leque de habilidades para estar na vanguarda da inovação tecnológica.
+    `,
+    
+  },
+  {
+    title: "CEO - Desenvolvedor de Sistemas",
+    contractType: "Patrick.developer",
+    logo: PatrickDeveloper, // Logo da empresa
+    companyName: "Patrick.developer",
+    description: `
+   
+    Patrick Gomes Siqueira é um experiente Desenvolvedor de Sistemas e CEO da sua própria empresa, especializada em soluções tecnológicas sob medida. Com vasta experiência em uma variedade de tecnologias e ferramentas, como HTML5, CSS3, Java, Spring Boot, C, C#, JavaScript, TypeScript, Node.js, React.js, e bancos de dados como MySQL, PostgreSQL e MariaDB, ele oferece desenvolvimento de aplicativos e websites robustos e eficientes. Além disso, possui expertise em AWS e Azure, garantindo soluções escaláveis e seguras para seus clientes. Patrick é apaixonado por transformar ideias em soluções inovadoras, sempre focado em entregar projetos de alta qualidade e excelência.
     `,
     
   },
